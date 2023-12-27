3a57d10c93f702622e116d25b805c12edc9cfc36
arseniy <snow_2015@list.ru>
2023-12-27 20:24:46 +0300

    3d space, moving camera, controls

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,12 @@
 #include "managers/texturemanager.h"
 #include "managers/shadermanager.h"
 #include "managers/buffermanager.h"
+#include "movingcamera.h"
 
-void handleEvents(SDL_Event& e, SDL_Window* window, bool& quit);
+void handleEvents(SDL_Event& e, SDL_Window* window, bool& quit, MovingCamera& movingCamera);
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
 
 int main(int argc, char* argv[])
 {
@@ -26,7 +30,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Program", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("Program", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCR_WIDTH, SCR_HEIGHT, SDL_WINDOW_OPENGL);
     if (window == NULL) {
         std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
         return 1;
@@ -39,36 +43,60 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+
+    // configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+    
     const GLubyte* version = glGetString(GL_VERSION);
     printf("OpenGL version: %s\n", version);
+
+    // main part 
+    // ---------
 
     TextureManager textureManager;
     ShaderManager shaderManager; 
     BufferManager bufferManager; 
+    MovingCamera movingCamera;
 
     SDL_Event e;
     bool quit = false;
 
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+    Uint32 frameStart;
+    int frameTime;
+
+    glm::mat4 projection = glm::mat4(1.0f);
+    GLuint projectionLocation = shaderManager.getUniform("projection");
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    GLuint modelLocation = shaderManager.getUniform("model");
+    GLuint viewLocation = shaderManager.getUniform("view");
+
     while (!quit) {
-        handleEvents(e, window, quit);
-
+        frameStart = SDL_GetTicks();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        handleEvents(e, window, quit, movingCamera);
 
-        // create transformations
-        glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        float timeValue = (float)SDL_GetTicks64() / 1000.0f;
-        float xValue = sin(timeValue) * 0.5f;
-        float yValue = sin(timeValue) * cos(timeValue) * 0.5f;
-        transform = glm::translate(transform, glm::vec3(xValue, yValue, 0.0f));
-        transform = glm::rotate(transform, timeValue, glm::vec3(1.0f, 1.0f, 0.0f));
+        // transformations
+        // ---------------
+        view = movingCamera.getViewMatrix();
         
-        unsigned int transformLoc = shaderManager.getUniform("transform");
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-        
+        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+
         bufferManager.updateBuffers();
-
         SDL_GL_SwapWindow(window);
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
 
     SDL_GL_DeleteContext(glcontext);
@@ -78,13 +106,30 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void handleEvents(SDL_Event& e, SDL_Window* window, bool& quit) {
+void handleEvents(SDL_Event& e, SDL_Window* window, bool& quit, MovingCamera& movingCamera) {
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    movingCamera.handleKeys(state);
+
+    int xrel, yrel;
+    SDL_GetRelativeMouseState(&xrel, &yrel);
+    movingCamera.handleMouseDirection(xrel, yrel);
+    
     while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-            else if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_RESIZED) {
-                glViewport(0, 0, e.window.data1, e.window.data2);
+        if (e.type == SDL_QUIT) {
+            quit = true;
+        }
+        else if (e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_RESIZED) {
+            glViewport(0, 0, e.window.data1, e.window.data2);
+        }
+        else if (e.type == SDL_KEYDOWN) {
+            if (e.key.keysym.sym == SDLK_ESCAPE) {
+                SDL_SetRelativeMouseMode(SDL_FALSE);
             }
         }
+        else if (e.type == SDL_MOUSEBUTTONDOWN) {
+            if (e.button.button == SDL_BUTTON_LEFT) {
+                SDL_SetRelativeMouseMode(SDL_TRUE);
+            }
+        }
+    }
 }
--- src/managers/buffermanager.cpp
+++ src/managers/buffermanager.cpp
@@ -1,35 +1,65 @@
 #include "buffermanager.h"
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 BufferManager::BufferManager()
 {
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
     float vertices[] = {
-        // positions          // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
-    };
-    // world space positions of our cubes
-    unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    //glGenBuffers(1, &EBO);
+  
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -43,11 +73,11 @@ BufferManager::~BufferManager()
 {
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    //glDeleteBuffers(1, &EBO);
 }
 
 void BufferManager::updateBuffers()
 {
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
